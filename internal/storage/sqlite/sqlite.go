22ac5ebffe055ec940efd2de35d04b729c41783a
olegopro <evilgazz@gmail.com>
2025-02-09 00:48:35 +0300

    Добавил сохранение URL в SQLite
    - Реализовал метод SaveURL для сохранения URL и алиаса в базу данных
    - Добавил обработку ошибки при нарушении уникальности алиаса
    - Использовал sqlite3.Error для проверки уникальности алиаса
    - Добавил возврат ID последней вставленной записи

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -3,8 +3,9 @@ package sqlite
 import (
 	"database/sql"
 	"fmt"
+	"url-shortener/internal/storage"
 
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/mattn/go-sqlite3"
 )
 
 type Storage struct {
@@ -19,7 +20,7 @@ func New(storagePath string) (*Storage, error) {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	stmt, err := db.Prepare(`
+	stmt, err := db.Prepare( /* sql */ `
 		CREATE TABLE IF NOT EXISTS url(
 			id INTEGER PRIMARY KEY,
 			alias TEXT NOT NULL UNIQUE,
@@ -37,4 +38,29 @@ func New(storagePath string) (*Storage, error) {
 	}
 
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
+	const op = "storage.sqlite.SaveURL"
+
+	stmt, err := s.db.Prepare( /* sql */ `INSERT INTO url(url, alias) VALUES(?, ?)`)
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	res, err := stmt.Exec(urlToSave, alias)
+	if err != nil {
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrURLExists)
+		}
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: failed to get last insert id: %w", op, err)
+	}
+
+	return id, nil
 }
