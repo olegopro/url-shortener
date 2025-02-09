8aac257119759f900f1e97b8a3840d2631b7a5e4
olegopro <evilgazz@gmail.com>
2025-02-09 16:47:07 +0300

    Добавил обработку запросов и генерацию случайных строк
    - Добавлены новые зависимости в go.mod и go.sum
    - Реализована обработка запросов в save.go:
    - Добавлена валидация запросов
    - Добавлена генерация случайных строк для alias, если он не указан
    - Созданы файлы для генерации случайных строк:
    - random.go с функцией NewRandomString
    - random_test.go с тестами для NewRandomString

--- internal/http-server/handlers/url/save/save.go
+++ internal/http-server/handlers/url/save/save.go
@@ -1,8 +1,17 @@
 package save
 
 import (
+	"errors"
+	"io"
 	"log/slog"
 	"net/http"
+	resp "url-shortener/internal/lib/api/response"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/lib/random"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
 )
 
 type Request struct {
@@ -11,10 +20,11 @@ type Request struct {
 }
 
 type Response struct {
-	Status string `json:"status"`
-	Error  string `json:"error,omitempty"`
-	Alias  string `json:"alias,omitempty"`
+	resp.Response
+	Alias string `json:"alias,omitempty"`
 }
+
+const aliasLength = 6
 
 type URLSaver interface {
 	SaveURL(urlToSave string, alias string) (int64, error)
@@ -23,5 +33,48 @@ type URLSaver interface {
 func New(log *slog.Logger, urlSaver URLSaver) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Логика обработчика
+		const op = "handler.url.save.New"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+		if errors.Is(err, io.EOF) {
+			// Такую ошибку встретим, если получили запрос с пустым телом.
+			// Обработаем её отдельно
+			log.Error("request body is empty")
+
+			render.JSON(w, r, resp.Error("empty request"))
+
+			return
+		}
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to decode request"))
+
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			validateErr := err.(validator.ValidationErrors)
+
+			log.Error("invalid request", sl.Err(err))
+
+			render.JSON(w, r, resp.ValidationError(validateErr))
+
+			return
+		}
+
+		alias := req.Alias
+		if alias == "" {
+			alias = random.NewRandomString(aliasLength)
+		}
 	}
 }
