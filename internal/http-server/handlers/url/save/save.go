163e9324a2cf03822d5d59adbb9fa0c4250bfd24
olegopro <evilgazz@gmail.com>
2025-02-09 15:26:44 +0300

    Создал базовые структуры для обработки и валидации URL
    - Добавил структуры Request и Response для эндпоинта сохранения URL
    - Реализовал интерфейс URLSaver для сохранения URL в хранилище
    - Создал пакет response для стандартизации ответов API
    - Добавил функции для обработки ошибок валидации запросов
    - Подготовил базовую структуру для обработчика сохранения URL

---
+++ internal/http-server/handlers/url/save/save.go
@@ -0,0 +1,27 @@
+package save
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+type Request struct {
+	URL   string `json:"url" validate:"required,url"`
+	Alias string `json:"alias,omitempty"`
+}
+
+type Response struct {
+	Status string `json:"status"`
+	Error  string `json:"error,omitempty"`
+	Alias  string `json:"alias,omitempty"`
+}
+
+type URLSaver interface {
+	SaveURL(urlToSave string, alias string) (int64, error)
+}
+
+func New(log *slog.Logger, urlSaver URLSaver) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Логика обработчика
+	}
+}
